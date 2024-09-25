edc6098a862c9dfffc83a5ba4996cb788d022522
Hazrat Mosaddique Ali <hazrat2325@gmail.com>
2024-09-25 17:09:06 +0000

    fira Code font added

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,12 @@
+//const tinderUser = new Object() | this is singleton object
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "rumi"
+tinderUser.isLoggedIn = false
+
+
+
+console.log(tinderUser);
+
+
