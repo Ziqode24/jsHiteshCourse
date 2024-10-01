785f1cdbe361a9ed0bc29d162110b8be7b434e6b
Hazrat Mosaddique Ali <hazrat2325@gmail.com>
2024-10-01 06:14:18 +0000

    error

--- 07_projects/04_GuessTheNumber/script.js
+++ 07_projects/04_GuessTheNumber/script.js
@@ -1,17 +1,16 @@
 let randomNumber = parseInt(Math.random() * 100 + 1);
-// console.log(randomNumber);
-
 const userGuess = document.getElementById("guess");
 const submitButton = document.getElementById("submit");
 const userMessages = document.getElementById("message");
 const guessSlot = document.getElementById("previous-guess");
 const tryRemain = document.getElementById("try-left");
 const hiddenButton = document.getElementsByClassName("reset-game")[0];
-const hiddenPara = document.getElementsByClassName("reset-para")[0]; //there must be the index to target specificly
+const hiddenPara = document.getElementsByClassName("reset-para")[0]; 
 
 let prevGuess = [];
 let numAttempts = 0;
 let playgame = true;
+
 hiddenButton.style.display = "none";
 hiddenPara.style.display = "none";
 
@@ -19,14 +18,13 @@ if (playgame) {
   submitButton.addEventListener("click", (e) => {
     e.preventDefault();
     const guess = parseInt(userGuess.value);
-    validateGame(guess);
-    tryRemain.innerText = '';
-    userMessages.innerText = '';
-    guessSlot.innerText = ''
+    if (!isNaN(guess)) {
+      validateGame(guess);  
+    }
   });
 }
 
-function validateGame() {
+function validateGame(guess) {
   if (guess < 1) {
     displayMessage("Oops, too low! Please enter a number between 1 and 100");
   } else if (guess > 100) {
@@ -35,13 +33,11 @@ function validateGame() {
     prevGuess.push(guess);
     if (numAttempts === 10) {
       displayGuess(guess);
-      displayMessage(
-        `Out of tries! The secret number was ${randomNumber}.Better luck next time!`
-      );
+      displayMessage(`Out of tries! The secret number was ${randomNumber}. Better luck next time!`);
       endGame();
     } else {
-      displayGuess();
-      checkGuess();
+      displayGuess(guess);  
+      checkGuess(guess);    
     }
   }
 }
@@ -59,36 +55,41 @@ function checkGuess(guess) {
 }
 
 function displayGuess(guess) {
-  userGuess = "";
-  guessSlot += `${guess}, `;
+  // Clear the slot before adding new guesses
+  if (numAttempts === 0) {
+    guessSlot.innerText = "";  // Clear the "None yet" message on the first guess
+  }
+  guessSlot.innerText += `${guess}, `;  // Append the new guess
   numAttempts++;
-  tryRemain.innerText = `10 - ${numAttempts}`;
+  tryRemain.innerText = `Tries Left: ${10 - numAttempts}`;
+  userGuess.value = "";  // Clear the input field after each guess
 }
+
 function displayMessage(message) {
   userMessages.innerText = `${message}`;
 }
 
 function endGame() {
-  userGuess = "";
-  userGuess.setAttribute("disabled", "");
-  hiddenButton.style.display = "block";
-  hiddenPara.style.display = "block";
-  playgame(false);
-  resetGame();
+  userGuess.value = "";  
+  userGuess.setAttribute("disabled", "");  
+  hiddenButton.style.display = "block";    
+  hiddenPara.style.display = "block";      
+  playgame = false;  
 }
 
 function resetGame() {
-  const resetGameButton = hiddenButton;
-  resetGameButton.addEventListener("click", function () {
-    randomNumber = parseInt(Math.random() * 100 + 1);
+  hiddenButton.addEventListener("click", function () {
+    randomNumber = parseInt(Math.random() * 100 + 1);  
     prevGuess = [];
     numAttempts = 0;
     tryRemain.innerText = "Still plenty of chances!";
     userMessages.innerText = "Start guessing!";
-    prevGuess.innerText = "None yet. C'mon, take a guess!";
-    userGuess.removeAttribute("disabled");
-    hiddenButton.style.display = "hidden";
-    hiddenPara.style.display = "hidden";
-    playgame(true);
+    guessSlot.innerText = "None yet. C'mon, take a guess!";
+    userGuess.removeAttribute("disabled");  
+    hiddenButton.style.display = "none";    
+    hiddenPara.style.display = "none";      
+    playgame = true;  
   });
 }
+
+resetGame();
