4e913d669b82ac755499f9f253ffb01332bcaa5a
Hazrat Mosaddique Ali <hazrat2325@gmail.com>
2024-10-05 05:18:30 +0000

    dark magic

---
+++ 09_advance/02_promises.js
@@ -0,0 +1,5 @@
+// promises
+//  the promise object representd the eventual completion (or failure) of an asyncronous operation & its resuloting value
+//   examples: [give 6 7 ex]
+
+//hab three states , pending : , fulfilled: , rejected: ,
